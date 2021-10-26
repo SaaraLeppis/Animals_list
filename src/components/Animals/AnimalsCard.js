e07b9fa6d41f722a9746f3460a489cb987c1a107
Saara <bc-macair027@BC-macair027s-MacBook-Air.local>
2021-10-26 15:17:22 +0300

    first version

---
+++ src/components/Animals/AnimalsCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AnimalsList from "./AnimalsList";
+
+const AnimalsCard = (props) => {
+    return (
+        <div className="card">
+            <h2>{props.name}</h2>
+            <img src=
+                {`https://source.unsplash.com/1600x900/?${props.name}`} />
+        </div>
+    )
+}
+export default AnimalsCard;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AnimalList from "./Animals/AnimalsList";
+
+const Main = () => {
+    return (
+        <div>
+            <AnimalList />
+        </div>
+    )
+}
+
+export default Main;
